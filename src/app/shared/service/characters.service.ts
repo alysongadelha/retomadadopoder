72c120da17731c1703a974224e284193106acaea
AlysonGadelha <alysongadelha.dev@gmail.com>
2022-01-25 14:46:21 -0300

    service finalized

--- src/app/shared/service/characters.service.ts
+++ src/app/shared/service/characters.service.ts
@@ -2,13 +2,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CharactersVampire } from './model/character-vampire.model';
+import { ResponsePageable } from './model/responsePageble.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharactersService {
-  constructor(public httpClient: HttpClient) {}
-  apiUrl = 'http://localhost:3358/api/CharVampireController/filiation/';
+  constructor(private httpClient: HttpClient) {}
+  apiUrl = 'http://localhost:3358/api/CharVampireController';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -16,6 +17,16 @@ export class CharactersService {
   };
 
   public getCharactersWithFlag(flag: string): Observable<CharactersVampire> {
-    return null;
+    return this.httpClient.get<CharactersVampire>(
+      `${this.apiUrl}/filiation/${flag}`
+    );
+  }
+
+  public getCharactersWithFlagPaginator(): Observable<ResponsePageable> {
+    return this.httpClient.get<ResponsePageable>(`${this.apiUrl}`);
+  }
+
+  public getAllChracters(): Observable<CharactersVampire[]> {
+    return this.httpClient.get<CharactersVampire[]>(`${this.apiUrl}`);
   }
 }
