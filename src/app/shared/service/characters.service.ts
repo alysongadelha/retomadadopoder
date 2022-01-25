d621b22004fc08c3240bcaf40856df232d8882dd
AlysonGadelha <alysongadelha.dev@gmail.com>
2022-01-25 14:02:18 -0300

    serice begin

--- src/app/shared/service/characters.service.ts
+++ src/app/shared/service/characters.service.ts
@@ -1,14 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharactersService {
+  constructor(public httpClient: HttpClient) {}
+  apiUrl = 'http://localhost:3358/api/CharVampireController/filiation/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
-  constructor() { 
-    const url = 'localhost:1234/bc';
+  public getCharactersWithFlag(flag: string): Observable<Characters> {
+    return null;
   }
-  
-  
-  
 }
