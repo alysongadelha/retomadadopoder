9a5937a5dba91c95a79b9ffe2f364ba9114b69fa
AlysonGadelha <alysongadelha.dev@gmail.com>
2022-01-25 14:36:20 -0300

    SAVEPOINT

--- src/app/shared/service/characters.service.ts
+++ src/app/shared/service/characters.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CharactersVampire } from './model/character-vampire.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +15,7 @@ export class CharactersService {
     }),
   };
 
-  public getCharactersWithFlag(flag: string): Observable<Characters> {
+  public getCharactersWithFlag(flag: string): Observable<CharactersVampire> {
     return null;
   }
 }
