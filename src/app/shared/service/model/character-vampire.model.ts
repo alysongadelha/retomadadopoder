4acb9d34f7dac41c7eb79895b212d7e13fe17602
AlysonGadelha <alysongadelha.dev@gmail.com>
2022-01-25 14:19:04 -0300

    sving models

---
+++ src/app/shared/service/model/character-vampire.model.ts
@@ -0,0 +1,12 @@
+export interface CharactersVampire {
+  name: string;
+  clan: string;
+  player?: string;
+  resume: string;
+  imgLink: string;
+  symbolLink: string;
+  dead: boolean;
+  register: string;
+  filiatiom: number;
+  generation?: number
+}
